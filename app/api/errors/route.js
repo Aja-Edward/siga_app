8e3e45d98d2b8d636882bc1d25f3471622f16138
Aja-Edward <77103145+Aja-Edward@users.noreply.github.com>
2023-11-04 16:03:33 +0100

    error removed

--- app/api/errors/route.js
+++ app/api/errors/route.js
@@ -1,12 +1,12 @@
-import { createEdgeRouter } from 'next-connect'
-import { connectToDB } from '@utils/database'
-import onError from '@backend/middlewares/errors'
-import { GET as GET_ROUTE } from '@/ourservices/route'
+// import { createEdgeRouter } from 'next-connect'
+// import { connectToDB } from '@utils/database'
+// import onError from '@backend/middlewares/errors'
+// import { GET as GET_ROUTE } from '@/ourservices/route'
 
-const router = createEdgeRouter(onError)
-router.get(GET_ROUTE)
-connectToDB()
+// const router = createEdgeRouter(onError)
+// router.get(GET_ROUTE)
+// connectToDB()
 
-export async function GET(request, cxt) {
-  return router.run(request, cxt)
-}
+// export async function GET(request, cxt) {
+//   return router.run(request, cxt)
+// }
