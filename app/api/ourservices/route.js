164ccac998da5cd7ee1baeeda80b7512905f0711
Aja-Edward <77103145+Aja-Edward@users.noreply.github.com>
2023-11-05 06:25:29 +0100

    mongoose updated

--- app/api/ourservices/route.js
+++ app/api/ourservices/route.js
@@ -1,13 +1,13 @@
 import { connectToDB } from '@utils/database'
 import Service from '@models/serviceModel'
-// import onError from '@backend/middlewares/errors'
 import { createRouter } from 'next-connect'
 
 export const router = createRouter()
 export const GET = async (request, cxt) => {
   router.run(request, cxt)
-  // const { q: query } = request.query
+
   try {
+    console.log(process.env.MONGODB_URI)
     await connectToDB()
 
     const services = await Service.find({})
